737c26d35187ed07ef122519e75b9b939ac505e9
Wenfei Zhou <wzhou2046@gmail.com>
2020-03-26 20:57:24 -0400

    last commit, please change icon and style the css and refactor some of my code. I stored a lot of things in global. In addition, execute this extension away from github folder. The .git file interferes with the tab functionality, so it seems

--- content.js
+++ content.js
@@ -1,11 +1,19 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "get_images") {
-    //wait for the tab api request from popup.js, if received then begin to select images on the current page
-    let formattedImages = [];
-    let images = $("img").map((i, img) => {
-      if ($(img).data("src")) return { src: $(img).data("src") };
-      else if ($(img).src) return { src: $(img).src };
+    let imgArr = [];
+
+    let images = $("img").map((item, img) => {
+      if ($(img).data("src")) {
+        imgArr.push({
+          src: $(img).data("src")
+        });
+      } else if ($(img).src) {
+        imgArr.push({
+          src: $(img).src
+        });
+      }
     });
-    sendResponse(images);
+
+    sendResponse(imgArr);
   }
 });
--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,19 @@
+let images = [];
+
 chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
   chrome.tabs.sendMessage(tabs[0].id, { action: "get_images" }, response => {
-    console.log(response);
+    $(".gallery").html("");
+
+    images = response;
+    response.map(img => {
+      $(".gallery").append('<img src="' + img.src + '" />');
+    });
+  });
+});
+
+$(document).on("click", "#download_all", e => {
+  console.log("Downloading all");
+  chrome.runtime.sendMessage({ action: "download", data: images }, res => {
+    console.log("Complete");
   });
 });
